5d6fdf159c0792d899b2715c9a2fa24aae680cff
rob-ng <rob.nate.grove@gmail.com>
2019-12-20 13:01:08 -0800

    Add missing documentation to `error` module

--- src/error.rs
+++ src/error.rs
@@ -1,24 +1,33 @@
 use std::error::Error as StdError;
 use std::fmt::{self, Debug, Display};
 
+/// Error that can occur during parsing.
 #[derive(Debug)]
 pub struct Error {
+    /// Line in CSV where error occured.
     line: usize,
+    /// Type of error.
     kind: ErrorKind,
 }
 
 impl Error {
-    pub fn new(line: usize, kind: ErrorKind) -> Self {
+    pub(crate) fn new(line: usize, kind: ErrorKind) -> Self {
         Error { line, kind }
     }
 }
 
+/// Kinds of error that can occur during parsing.
 #[derive(Debug)]
 pub enum ErrorKind {
+    /// Error that occurs due to field being malformed.
     BadField { col: usize, msg: String },
+    /// Error that occurs due to record having too few or too many fields.
     UnequalNumFields { expected_num: usize, num: usize },
+    /// Error that occurs due to record exceeding max number of bytes.
     RecordTooLarge { max_record_size: usize },
+    /// Error that occurs due to problems reading CSV.
     Io(std::io::Error),
+    /// Error that occurs when CSV contains non-UTF-8 values.
     Utf8(std::str::Utf8Error),
 }
 
